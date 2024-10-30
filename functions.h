6ea7e631e2a55e656fa1af5166b3fd960ae54fac
cnxrlie <cholden@terpmail.umd.edu>
2024-10-30 14:06:03 -0400

    changing max from 100 to 101111

--- functions.h
+++ functions.h
@@ -2,7 +2,7 @@
 #ifndef MATRIX_OPERATIONS_H
 #define MATRIX_OPERATIONS_H
 
-#define MAX_SIZE 100  // Maximum matrix size
+#define MAX_SIZE 101111  // Maximum matrix size
 
 // Function prototypes
 void transposeMatrix(int original[][MAX_SIZE], int transposed[][MAX_SIZE], int row, int col);
